8dd385095e424b58de349d0ac69dd35ee550b097
Camila de Oliveira Romão Brandão <camila.brandao@olrretail.com>
2020-11-27 15:08:58 +0000

    Revert "[SP] Implemented Org Stores."
    This reverts commit 673c11535fbc9f8ea1c37bacac62aeda681fb626.
    
    Change-Id: Ifed6942919f8993e7906bb17f8442d5260334b2b

--- features/support/pages/shared.rb
+++ features/support/pages/shared.rb
@@ -27,24 +27,12 @@ module Pages
     element(:loading_list) { div(:class, 'AFAutoSuggestBusyStyle') }
     element(:cancel_button) { a(:text, 'Cancel') }
 
-    element(:tasks_button) { a(:title, 'Tasks') }
-    element(:organizational_Hierarchy) { span(:text, 'Organizational Hierarchy') }
-
-    ## Possible Save and Closes ##
-    element(:save_and_close_01) { a(:text, /Save and Close/) }
-
     ############################### Methods ###############################
 
     def save_and_close
-      sleep 1
-      if save_and_close_button.present?
-        TryWith.attempts(attempts: 3, sleep: 2) do
-          save_and_close_button.wait_until_present.click
-          wait_for_db_activity
-        end
-      elsif save_and_close_01.present?
+      TryWith.attempts(attempts: 3, sleep: 2) do
+        save_and_close_button.wait_until_present.click
         wait_for_db_activity
-        save_and_close_01.click
       end
     end
 
@@ -66,45 +54,10 @@ module Pages
       if no_results.present?
       else
         @actual_values = result_list.text
-        raise "Values were not as expected.\nExpected:\n#{expected_values}\nActual:\n#{@actual_values}" unless @actual_values.include? expected_values
+        raise "Values were not as expected.\nExpected:\n#{expected_values}\nActual:\n#{@actual_values}" unless @actual_values.include?expected_values
       end
     end
 
-    #
-    ## Filter - img(alt: 'Query by Example') ##
-    def filter_activity(filter_element, filter_data)
-      ## Filter ##
-      if (filter_element.present? == true)
-        filter_element.send_keys filter_data
-        2.times do
-          filter_element.send_keys :enter
-        end
-      else
-        TE.browser.img(alt: 'Query by Example').click
-        filter_element.send_keys filter_data
-        wait_for_db_activity
-        2.times do
-          filter_element.send_keys :enter
-        end
-        wait_for_db_activity
-      end
-    end
-
-    def from_org_hierarchy
-      tasks_button.wait_until_present.click
-      wait_for_db_activity
-      organizational_Hierarchy.click
-      wait_for_db_activity
-    end
-
-
-    def enter_times(ele, times)
-      sleep 1
-      times.times do
-        ele.send_keys :enter
-      end
-      wait_for_db_activity
-    end
 
   end
 end
